9a2f998ce58ab74fecdd0260b909d85aa02af2e8
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-01-18 19:21:40 -0300

    :video_game: Adding SDL2 library

--- Sandbox/src/Main.cpp
+++ Sandbox/src/Main.cpp
@@ -1,9 +1,16 @@
 #include <Nexus.h>
 
-int main() {
+#include <SDL2/SDL.h>
+
+int main(int argc, char** argv) {
 	Nexus::Log::Init();
 
 	NEXUS_TRACE("Hello World from Sandbox!");
+
+	SDL_version version;
+	SDL_VERSION(&version);
+
+	NEXUS_INFO("I'm using the {}.{}.{} version of SDL2.", version.major, version.minor, version.patch);
 
 	return 0;
 }
--- premake5.lua
+++ premake5.lua
@@ -9,6 +9,10 @@ workspace "Nexus"
 proj_targetdir = "bin/%{cfg.buildcfg}-%{cfg.architecture}/%{prj.name}"
 proj_objdir = "bin-int/%{cfg.buildcfg}-%{cfg.architecture}/%{prj.name}"
 
+include_dir = {}
+include_dir["spdlog"] = "%{wks.location}/Nexus/vendor/spdlog/include"
+include_dir["SDL2"] = "%{wks.location}/Nexus/vendor/SDL2/include"
+
 project "Nexus"
 	location "Nexus"
 	kind "StaticLib"
@@ -28,13 +32,24 @@ project "Nexus"
 
 	includedirs {
 		"%{prj.name}/include",
-		"%{prj.name}/vendor/spdlog/include"
+		"%{include_dir.spdlog}",
+		"%{include_dir.SDL2}"
 	}
 
 	filter "system:windows"
 		cppdialect "C++17"
 		staticruntime "On"
 		systemversion "latest"
+
+		libdirs {
+			"%{prj.name}/vendor/SDL2/lib/%{cfg.architecture}"
+		}
+
+		links {
+			"SDL2",
+			"SDL2main"
+		}
+
 
 project "Sandbox"
 	location "Sandbox"
@@ -50,8 +65,9 @@ project "Sandbox"
 	}
 
 	includedirs {
-		"Nexus/include",
-		"Nexus/vendor/spdlog/include"
+		"%{wks.location}/Nexus/include",
+		"%{include_dir.spdlog}",
+		"%{include_dir.SDL2}"
 	}
 
 	links {
