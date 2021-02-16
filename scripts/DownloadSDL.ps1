0248b0588a10ea535a53d43c326c0c5535c24fbf
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-02-16 14:27:30 -0300

    :toolbox: Creating SDL2 download script
    Leaving the repository smaller, people who will build on Linux or MacOS do not need to download libraries for Windows.

---
+++ scripts/DownloadSDL.ps1
@@ -0,0 +1,8 @@
+cd ..\Nexus\vendor
+
+wget https://libsdl.org/release/SDL2-devel-2.0.14-VC.zip -O SDL2.zip
+Expand-Archive .\SDL2.zip .
+
+mv .\SDL2-2.0.14\ SDL2
+mv .\SDL2/lib/x64 .\SDL2/lib/x86_64
+rm SDL2.zip
--- scripts/GenerateProjects.bat
+++ scripts/GenerateProjects.bat
@@ -1,3 +1,4 @@
+powershell -file DownloadSDL.ps1
 pushd %~dp0..\
 call vendor\premake\premake5.exe vs2019
 popd
